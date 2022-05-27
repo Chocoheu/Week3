29205d196469ce9b9099b0c6cf30fea170b62400
Chocoheu <105211245+Chocoheu@users.noreply.github.com>
2022-05-27 12:16:27 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -9,10 +9,11 @@ public class Week3 {
 
     /**
      * @param args the command line arguments
-     week 3 */
+     week 3 changes */
     public static void main(String[] args) 
     {
         System.out.println("Learning Github");
+        System.out.pritnln("changes in github");
     }
 
 }
