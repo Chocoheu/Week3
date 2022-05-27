66035976835343b3f44a7f5463c98058790f443e
Chocoheu <phaminhh@sheridancollege.ca>
2022-05-27 11:28:00 +0700

    version 1

---
+++ src/week3/Week3.java
@@ -0,0 +1,18 @@
+package week3;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Carly Pham
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Learning Github");
+    }
+
+}
